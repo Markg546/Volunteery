ca114d6947d58108958cd34c36a908e632152814
Mark <markg546@gmail.com>
2021-11-23 23:43:17 +0200

    Initial commit

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // Checking
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
+    },
+  },
+  // ...
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e6907f0d-4fd2-4299-bfa5-9e211251ceeb'
+};
